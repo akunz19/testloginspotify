24e08856300f5ef86e00aa5f1c63a7e061c72a63
amanda <amanda.m.kunz@gmail.com>
2019-10-22 21:24:31 -0500

    test

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var clientID = 'a0ee7d1e23dc4b14ac56e93bb2a390ce';
-var redirectURI= '';
-var queryURL= 'https://accounts.spotify.com/authorize?'+clientID+'&response_type=code';
+var redirectURI= 'https://akunz19.github.io/testloginspotify/';
+var queryURL= 'https://accounts.spotify.com/authorize?client_id='+clientID+'&response_type=token'+'&redirect_uri='+redirectURI;
 
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  })
+
+
+  $("button").on("click",  function(){
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+          console.log(response)
+      }).catch(function(error){
+          console.log(error)
+      })
+  });
